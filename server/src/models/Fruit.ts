14afb4b86c571e2266b02da12b3287e7fd7e44c2
bangberga <bangberga@gmail.com>
2022-11-16 12:18:25 +0700

    Fruit schema

--- server/src/models/Fruit.ts
+++ server/src/models/Fruit.ts
@@ -1,10 +1,10 @@
 import { Schema, model, ValidatorProps, SchemaDefinition } from "mongoose";
 
-const def: SchemaDefinition = {
+const schemaDefinition: SchemaDefinition = {
   name: {
     type: String,
     required: [true, "Please provide fruit name"],
-    maxLength: [20, "Fruit name length must <= 20"],
+    maxlength: [20, "Fruit name length must be <= 20"],
     unique: true,
     trim: true,
   },
@@ -33,8 +33,12 @@ const def: SchemaDefinition = {
     default:
       "https://firebasestorage.googleapis.com/v0/b/fruit-selling-management.appspot.com/o/unavailable%20image.jpg?alt=media&token=f26215c3-2fb8-42d9-ba4c-937f46aeea1",
   },
+  description: {
+    type: String,
+    maxlength: [255, "Description length must be <= 255 characters"],
+  },
 };
 
-const FruitSchema = new Schema(def, { timestamps: true });
+const FruitSchema = new Schema(schemaDefinition, { timestamps: true });
 
 export default model("Fruit", FruitSchema);
